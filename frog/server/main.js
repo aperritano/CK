267e4e91529bc93179bdd891f7306e4f90a21c22
Stian Håklev <shaklev@gmail.com>
2017-06-22 07:51:09 +0200

    Should fail eslint

--- frog/server/main.js
+++ frog/server/main.js
@@ -3,6 +3,7 @@
 import { Meteor } from 'meteor/meteor';
 
 import { startShareDB } from './share-db-manager';
+import { startShareDB } from './share-db-manager';
 
 import '../imports/startup/shutdown-if-env.js';
 
